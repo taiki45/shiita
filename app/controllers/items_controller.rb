b66f081ae138321ce8d2a145e30130462c363a35
Taiki ONO <taiks.4559@gmail.com>
2013-04-20 01:51:06 +0900

    fix scaffolld bug for mongoid

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.to_a
 
     respond_to do |format|
       format.html # index.html.erb
