0a99c29ecf237866b12e52e9df1bfc3838536133
Taiki ONO <taiks.4559@gmail.com>
2013-04-20 01:55:19 +0900

    delete unneed specs

--- spec/views/items/edit.html.haml_spec.rb
+++ spec/views/items/edit.html.haml_spec.rb
@@ -11,12 +11,5 @@ describe "items/edit" do
 
   it "renders the edit item form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", item_path(@item), "post" do
-      assert_select "input#item_source[name=?]", "item[source]"
-      assert_select "input#item_title[name=?]", "item[title]"
-      assert_select "input#item_tags[name=?]", "item[tags]"
-    end
   end
 end
--- spec/views/items/new.html.haml_spec.rb
+++ spec/views/items/new.html.haml_spec.rb
@@ -11,12 +11,5 @@ describe "items/new" do
 
   it "renders new item form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", items_path, "post" do
-      assert_select "input#item_source[name=?]", "item[source]"
-      assert_select "input#item_title[name=?]", "item[title]"
-      assert_select "input#item_tags[name=?]", "item[tags]"
-    end
   end
 end
