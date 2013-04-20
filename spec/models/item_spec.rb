337b93d6be5cdd896ca3ee2fcf03bc34c5cbe506
Taiki ONO <taiks.4559@gmail.com>
2013-04-21 03:41:04 +0900

    redescribe Item using mongoid matchers

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,21 +2,11 @@ require 'spec_helper'
 
 describe Item do
 
-  describe "#created_at" do
-    subject { create :item }
-    its(:created_at) { should be_within(10.seconds).of(Time.now) }
-  end
-
-  describe "#updated_at" do
-    subject { create :item }
-    its(:updated_at) { should be_within(10.seconds).of(Time.now) }
-
-    it "should updated when updated its value" do
-      before = subject.updated_at
-      subject.tap {|item| item.title = "updated" }.save
-      expect(subject.updated_at).not_to eq before
-    end
-  end
+  it { should have_field(:title).of_type(String) }
+  it { should have_field(:source).of_type(String) }
+  it { should have_field(:tags).of_type(Array) }
+  it { should be_timestamped_document }
+  it { should belong_to(:user) }
 
   describe "#user" do
     subject { create :item }
