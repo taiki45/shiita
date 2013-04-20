25fbbc56580e9cff3a83c911b6545543e5a30f0c
Taiki ONO <taiks.4559@gmail.com>
2013-04-21 01:58:50 +0900

    remove duplicated specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,26 +18,6 @@ describe User do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:email) }
 
-  describe "validations" do
-    context "when it has unvalid data" do
-      its(:valid?) { should be false }
-    end
-
-    context "when it has valid data" do
-      subject { build :user }
-      its(:valid?) { should be true }
-    end
-  end
-
-  describe "#create" do
-    before { create :user }
-    subject { User.first }
-
-    its(:uid) { should eq 1 }
-    its(:name) { should eq "taiki" }
-    its(:email) { should eq "taiki@example.com" }
-  end
-
 
   let(:auth) do
     {
