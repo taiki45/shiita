3e9666faa24a9919c5ac62cd2553959c86a4f8c5
Taiki ONO <taiks.4559@gmail.com>
2013-04-21 04:12:59 +0900

    describe ApplicationController#login?

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe ApplicationController do
+
+  let(:user) { create :user }
+
   describe "#current_user" do
     subject { controller.__send__ :current_user }
-
-    let(:user) { create :user }
 
     context "with no user id in session" do
       it { should be_nil }
@@ -20,4 +21,19 @@ describe ApplicationController do
       it { should be_nil }
     end
   end
+
+  describe "#login?" do
+    subject { controller.__send__ :login? }
+
+    context "when user has logined" do
+      before { session[:id] = user.id }
+      it { should be_true }
+    end
+
+    context "when user has logouted" do
+      before { session[:id] = nil }
+      it { should be_false }
+    end
+  end
+
 end
