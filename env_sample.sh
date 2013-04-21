1ed74d8d1f4a64b982d31477b5ae1ccbe1ff0e57
Taiki ONO <taiks.4559@gmail.com>
2013-04-21 16:49:56 +0900

    fix typo

--- env_sample.sh
+++ env_sample.sh
@@ -5,7 +5,7 @@ export GOOGLE_SECRET="your_client_secret"
 # This is for debug login
 export SHIITA_DEV="your_local_development_user_id"
 
-# For your company's email domain such as "@copany.com".
+# For your company's email domain such as "@company.com".
 # Shiita uses this value for complement.
 # See app/controllers/users_controller.rb
 export SHIITA_DOMAIN="email_domain"
