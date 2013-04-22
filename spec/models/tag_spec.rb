5856e8c8722ead8b5acfe75a6f9f012de6ba6b53
Taiki ONO <taiks.4559@gmail.com>
2013-04-23 02:15:54 +0900

    describe Tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe Tag do
+
+  it { should have_field(:name).of_type(String) }
+  it { should_not be_timestamped_document }
+  it { should have_and_belong_to_many(:items).of_type(Item) }
+
+  describe "#to_param" do
+    subject { create :tag }
+    its(:to_param) { should eq subject.name }
+  end
+
 end
