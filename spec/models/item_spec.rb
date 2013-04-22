9c6a1716a6e0340054c73a47a19b46fa7ccca0fe
Taiki ONO <taiks.4559@gmail.com>
2013-04-23 01:42:13 +0900

    fix Item specs for new class Tag

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,21 +4,15 @@ describe Item do
 
   it { should have_field(:title).of_type(String) }
   it { should have_field(:source).of_type(String) }
-  it { should have_field(:tags).of_type(Array) }
   it { should be_timestamped_document }
-  it { should belong_to(:user) }
+  it { should belong_to(:user).of_type(User) }
+  it { should have_and_belong_to_many(:tags).of_type(Tag) }
 
   describe "#user" do
     subject { create :item }
 
     its(:user) { should_not be_nil }
     its("user.uid") { should eq 1 }
-  end
-
-  describe "#tags" do
-    subject { create :item }
-    its(:tags) { should have(2).tags }
-    its(:tags) { should be_include "Ruby" }
   end
 
 end
