b3f87aefbc4b9747a87537f4b14c80b0a9a88173
Taiki ONO <taiks.4559@gmail.com>
2013-04-23 23:48:09 +0900

    convert from markdown with redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def markdown(text)
+    redcarpet.render(text).html_safe
+  end
+
+  private
+
+  def redcarpet
+    @redcarpet ||= Redcarpet::Markdown.new(
+      SyntaxHighlighting.new(
+        filter_html: true,
+        hard_wrap:true
+      ),
+      fenced_code_blocks: true,
+      autolink: true
+    )
+  end
 end
