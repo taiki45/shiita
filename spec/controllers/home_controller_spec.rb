43f836122a7f6711dbf185d89289080e4b3168bc
Taiki ONO <taiks.4559@gmail.com>
2013-04-28 03:18:35 +0900

    describe controller of home#index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,43 @@ require 'spec_helper'
 
 describe HomeController do
 
+  before do
+    @item = create :item
+    @user = @item.user
+  end
+
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    context "with no login" do
+      it "returns http success" do
+        get :index
+        response.should be_success
+      end
+
+      it "assigns true to @no_sidebar" do
+        get :index
+        assigns(:no_sidebar).should be_true
+      end
+    end
+
+    context "with login" do
+      let(:user) { create :user }
+      before do
+        tag = create(:tag).tap {|e| e.items.push(@item); e.save }
+        @user.tags.push tag
+        @user.save
+        session[:id] = @user.id
+        controller.stub(:current_user) { @user }
+      end
+
+      it "assigns flase to no_sidebar" do
+        get :index
+        assigns(:no_sidebar).should be_false
+      end
+
+      it "assigns all following items" do
+        get :index
+        assigns(:items).should eq @user.following_items
+      end
     end
   end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :item do
     title "Ruby on Rails tips"
     source "Rails is cool"
-    association :user, factory: :user, strategy: :create
+    association :user, factory: :user
   end
 end
