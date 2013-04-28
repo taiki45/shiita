df33a8486b16969c34c18e8e943b2fcb1d082701
Taiki ONO <taiks.4559@gmail.com>
2013-04-28 21:20:02 +0900

    add user follow ajax proccessing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
   end
 
   def follow
-    @user = User.find_by_part_of(params[:email])
-    current_user.follow(@user)
-    flash[:notice] = "Success to follow #{@user.name}" if current_user.save && @user.save
-    render :show
+    @obj = User.find_by_part_of(params[:email])
+    current_user.follow(@obj)
+    if current_user.save && @obj.save
+      render "share/follow"
+    else
+      render "share/follow_error"
+    end
   end
 end
