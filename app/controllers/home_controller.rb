89f500bd9a488f44d5aaf49f954ea086bc4b4c38
Taiki ONO <taiks.4559@gmail.com>
2013-04-30 22:16:27 +0900

    add limitaion for User#following_items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     render :index and return unless login?
 
     @no_sidebar = false
-    @items = current_user.following_items
+    @items = current_user.following_items(20)
     @items = Item.order_by(updated_at: -1).limit(10).all if @items.empty?
     render :home
   end
