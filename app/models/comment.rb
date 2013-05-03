7cf62490f1ab68385286fda70f446a80ef55fa3c
Taiki ONO <taiks.4559@gmail.com>
2013-05-04 02:06:02 +0900

    delete Comment.new_with_user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,13 +11,6 @@ class Comment
   validates :content, presence: true
   validates :user_id, presence: true
 
-  class << self
-    def new_with_user(attrs)
-      attrs[:user_id] = attrs.delete(:user).id
-      new(attrs)
-    end
-  end
-
   def user
     User.find(user_id)
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,20 +11,11 @@ describe Comment do
   it { should validate_presence_of(:content) }
   it { should validate_presence_of(:user_id) }
 
-  describe ".new_with_user" do
-    let(:user) { create :user }
-    let(:valid_attrs) { {content: "test", user: user} }
-
-    subject { described_class.new_with_user(valid_attrs) }
-    it { should be_valid }
-    its(:user_id) { should eq user.id.to_s }
-  end
-
   describe "#user" do
     let(:user) { create :user }
-    let(:valid_attrs) { {content: "test", user: user} }
+    let(:valid_attrs) { {content: "test"} }
 
-    subject { described_class.new_with_user(valid_attrs).user }
+    subject { described_class.new(valid_attrs).tap {|o| o.user_id = user.id }.user }
     it { should eq user }
   end
 
