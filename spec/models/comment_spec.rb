962d4611c50aa4c5b3e010c339c1846ea1e5d39e
Taiki ONO <taiks.4559@gmail.com>
2013-05-04 02:14:18 +0900

    Revert "delete Comment.new_with_user"
    This reverts commit 7cf62490f1ab68385286fda70f446a80ef55fa3c.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,11 +11,20 @@ describe Comment do
   it { should validate_presence_of(:content) }
   it { should validate_presence_of(:user_id) }
 
+  describe ".new_with_user" do
+    let(:user) { create :user }
+    let(:valid_attrs) { {content: "test", user: user} }
+
+    subject { described_class.new_with_user(valid_attrs) }
+    it { should be_valid }
+    its(:user_id) { should eq user.id.to_s }
+  end
+
   describe "#user" do
     let(:user) { create :user }
-    let(:valid_attrs) { {content: "test"} }
+    let(:valid_attrs) { {content: "test", user: user} }
 
-    subject { described_class.new(valid_attrs).tap {|o| o.user_id = user.id }.user }
+    subject { described_class.new_with_user(valid_attrs).user }
     it { should eq user }
   end
 
