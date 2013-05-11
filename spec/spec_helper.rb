994f14ee178760c192f4a94028a9ca313f2c8d91
Taiki ONO <taiks.4559@gmail.com>
2013-05-12 00:40:02 +0900

    setup for spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,26 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'spork'
 require 'database_cleaner'
+
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+#
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
