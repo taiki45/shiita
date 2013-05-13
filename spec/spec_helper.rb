96082c504fdbe69e06f256a9772605d04eb2bd07
Taiki ONO <taiks.4559@gmail.com>
2013-05-14 00:36:32 +0900

    remove spork-rails and use spring for preloader

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,49 +3,32 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'spork'
 require 'database_cleaner'
 
-#uncomment the following line to use spork with the debugger
-#require 'spork/ext/ruby-debug'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+RSpec.configure do |config|
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
 
-Spork.prefork do
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
-  RSpec.configure do |config|
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
-    config.infer_base_class_for_anonymous_controllers = false
-
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
-    config.order = "random"
-
-    # Use database_cleaner
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.orm = "mongoid"
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.clean
-    end
-
-    config.include FactoryGirl::Syntax::Methods
-
+  # Use database_cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
   end
-end
 
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
 
-Spork.each_run do
-
-  # This code will be run each time you run your specs.
-  load "#{Rails.root}/config/routes.rb"
-  Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+  config.include FactoryGirl::Syntax::Methods
 
 end
