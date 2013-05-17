d0c8d54afca92326ed4d456db91fde83ef118180
Taiki ONO <taiks.4559@gmail.com>
2013-05-18 01:37:20 +0900

    update env_sample [ci skip]

--- env_sample.sh
+++ env_sample.sh
@@ -2,10 +2,7 @@
 export GOOGLE_KEY="your_client_key"
 export GOOGLE_SECRET="your_client_secret"
 
-# This is for debug login
-export SHIITA_DEV="your_local_development_user_id"
-
-# For your company's email domain such as "@company.com".
+# Fill your company's email domain such as "@company.com".
 # Shiita uses this value for complement.
 # See app/controllers/users_controller.rb
 export SHIITA_DOMAIN="email_domain"
