521bc540076fa8a8d25ef6b966963ab8824616b6
Taiki ONO <taiks.4559@gmail.com>
2013-05-18 23:41:43 +0900

    add sample envs

--- env_sample.sh
+++ env_sample.sh
@@ -6,3 +6,13 @@ export GOOGLE_SECRET="your_client_secret"
 # Shiita uses this value for complement.
 # See app/controllers/users_controller.rb
 export SHIITA_DOMAIN="email_domain"
+
+
+# In production at remote machine
+export SHIITA_USER="your system username"
+export SHIITA_GROUP="your system group"
+
+# For deploy at local machine
+export DEPROY_USER="remote user name"
+export DEPROY_SERVER="remote server name"
+export DEPROY_TO_DIR="remote dir for deploy"
