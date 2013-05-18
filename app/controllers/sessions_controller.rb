d048d8c031e477dcd4c41e038f444548205cd83c
Taiki ONO <taiks.4559@gmail.com>
2013-05-19 06:14:19 +0900

    add email domain validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def callback
+    unless User.validate_domain(request.env['omniauth.auth'])
+      flash[:error] = %(Use "#{Settings.email_domain}" email address to login.)
+      redirect_to(root_path) && return
+    end
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:id] = user.id
     flash[:notice] = "Success to login as #{user.name}."
