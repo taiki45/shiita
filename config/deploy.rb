b2781aac2a1b9f4f62fff184f49a2b59ebcca9be
Taiki ONO <taiks.4559@gmail.com>
2013-05-19 00:12:52 +0900

    fix deploy.rb for deploying shiita

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,72 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+def validate_env(*env_keys)
+  raise RuntimeError.new("Necessary ENVs are not defined") unless ENV.values_at(*env_keys).all?
+end
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+validate_env(*%w(
+  DEPROY_USER
+  DEPROY_SERVER
+  DEPROY_TO_DIR
+))
+
+
+set :application, "shiita"
+set :use_sudo, false
+set :user, ENV["DEPROY_USER"]
+set :deploy_to, ENV["DEPROY_TO_DIR"]
+set :rake, "rake RAILS_ENV=#{rails_env}"
+server ENV["DEPLOY_SERVER"], :web, :app, :db, primary: true
+
+set :scm, :git
+set :branch, "add-deploy-task"
+set :repository, "https://github.com/taiki45/shiita.git"
+set :deploy_via, :copy
+set :bundle_without, [:development, :test, :capistrano]
+set :keep_releases, 5
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :setup do
+  task :fix_permissions do
+    sudo "chown -R #{user}.#{user} #{deploy_to}"
+  end
+end
+after "deploy:setup", "setup:fix_permissions"
+
+namespace :deploy do
+
+  task :start, :roles => :app do
+    run "cd #{current_path} && bundle exec unicorn_rails -c config/unicorn.rb -E #{rails_env} -D"
+  end
+
+  task :restart, :roles => :app do
+    if File.exist? "tmp/pids/unicorn.pid"
+      run "cd #{current_path} && kill -s USR2 `cat tmp/pids/unicorn.pid`"
+    end
+  end
+
+  task :stop, :roles => :app do
+    run "cd #{current_path} && kill -s QUIT `cat tmp/pids/unicorn.pid`"
+  end
+
+  desc "Ensure indexes for MongoDB"
+  task :create_indexes, :roles => :db do
+    run "cd #{current_path} && #{rake} db:mongoid:create_indexes"
+  end
+
+  namespace :assets do
+    desc "Run the asset precompilation rake task if assets changed"
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      modified = capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      if previous_release.nil? || modified
+        run %Q{cd #{latest_release} && #{rake} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
