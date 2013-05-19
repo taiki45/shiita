ca2997f0c742f6ec105b807757d4a657248dec9b
Taiki ONO <taiks.4559@gmail.com>
2013-05-19 20:13:53 +0900

    describe users routings

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,8 +3,46 @@ require "spec_helper"
 describe UsersController do
   describe "routing" do
 
+    describe "constrains" do
+      it "routes with dot containing email" do
+        get("/users/test.1").should route_to("users#show", email: "test.1")
+      end
+
+      it "routes with minus containing email" do
+        get("/users/test-1").should route_to("users#show", email: "test-1")
+      end
+    end
+
     it "routes to #show" do
-      get("/users/test-t").should route_to("users#show", email: "test-t")
+      get("/users/test").should route_to("users#show", email: "test")
+    end
+
+    it "routes to #index" do
+      get("/users").should route_to("users#index")
+    end
+
+    it "routes to #follow" do
+      post("/users/test/follow").should route_to("users#follow", email: "test")
+    end
+
+    it "doesn't route to #follow with get" do
+      get("/users/test/follow").should_not route_to("users#follow", email: "test")
+    end
+
+    it "routes to #unfollow with delete" do
+      delete("/users/test/follow").should route_to("users#unfollow", email: "test")
+    end
+
+    it "routes to #stocks" do
+      get("/users/test/stocks").should route_to("users#stocks", email: "test")
+    end
+
+    it "routes to #followings" do
+      get("/users/test/followings").should route_to("users#followings", email: "test")
+    end
+
+    it "routes to #followers" do
+      get("/users/test/followers").should route_to("users#followers", email: "test")
     end
 
   end
