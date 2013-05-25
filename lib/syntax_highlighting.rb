5ed3ab40b0bec171c583cf5d31bd7e72418f7167
Taiki ONO <taiks.4559@gmail.com>
2013-05-25 19:24:23 +0900

    add codespan mathjax support

--- lib/syntax_highlighting.rb
+++ lib/syntax_highlighting.rb
@@ -1,10 +1,21 @@
 class SyntaxHighlighting < Redcarpet::Render::HTML
   def block_code(code, language)
-    if %w(tex asciimath mml).include? language
-        %(<script type="math/#{language}; mode=display">#{code}</script>)
+    if %w(tex latex).include? language
+        %(<script type="math/tex; mode=display">#{code}</script>)
     else
       language = :text unless CodeRay::Scanners.list.include? (language && language.to_sym)
       CodeRay.scan(code, language).div
+    end
+  end
+
+
+  def codespan(code)
+    if code.length > 2 && code[0] == "$" && code[-1] == "$"
+      code.gsub!(/^\$/,'')
+      code.gsub!(/\$$/,'')
+      "<script type=\"math/tex\">#{code}</script>"
+    else
+      "<code>#{code}</code>"
     end
   end
 end
