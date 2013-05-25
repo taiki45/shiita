2067cf7909f6bc05e8c5c7fd1a55e1e4f23a8ed1
Taiki ONO <taiks.4559@gmail.com>
2013-05-25 18:40:19 +0900

    add mathjax and fix codeblock parsing

--- lib/syntax_highlighting.rb
+++ lib/syntax_highlighting.rb
@@ -1,6 +1,10 @@
 class SyntaxHighlighting < Redcarpet::Render::HTML
   def block_code(code, language)
     language = :text unless language
-    CodeRay.scan(code, language).div
+    if language == 'mathjax'
+      %(<script type="math/tex; mode=display">#{code}</script>)
+    else
+      CodeRay.scan(code, language).div
+    end
   end
 end
